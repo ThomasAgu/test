63a1ec7057f611d29213223fd4da91e4ccd88381
ThomasAguado <63058489+ThomasAgu@users.noreply.github.com>
2023-09-29 21:19:50 -0300

    Delete variables_entorno.js

--- variables_entorno.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    DB_HOST: 'precios-1.c0f6dm2ucnlg.us-east-2.rds.amazonaws.com',
-    DB_PORT: 3306,
-    DB_USER: 'candidatoPrueba',
-    DB_PASSWORD: 'gaspre21.M',
-    DB_NAME: 'prueba',
-  };
